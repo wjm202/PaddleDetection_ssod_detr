a8524308ce020068f9a5d3c0841c1e248393424d
wjm202 <897383984@qq.com>
2023-01-16 04:51:21 +0000

    ssod_2dalign_contrastloss

--- ppdet/modeling/architectures/yolo.py
+++ ppdet/modeling/architectures/yolo.py
@@ -15,6 +15,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from typing_extensions import Self
 
 from ppdet.core.workspace import register, create
 from .meta_arch import BaseArch
@@ -43,7 +44,6 @@ class YOLOv3(BaseArch):
                  for_mot=False):
         """
         YOLOv3 network, see https://arxiv.org/abs/1804.02767
-
         Args:
             backbone (nn.Layer): backbone instance
             neck (nn.Layer): neck instance
@@ -61,7 +61,11 @@ class YOLOv3(BaseArch):
         self.for_mot = for_mot
         self.return_idx = isinstance(post_process, JDEBBoxPostProcess)
         self.is_teacher = False
-
+        self.queue_ptr=0
+        self.queue_size = 100*672
+        self.queue_feats = paddle.zeros([self.queue_size, 80]).cuda()
+        self.queue_probs = paddle.zeros([self.queue_size, 80]).cuda()
+        self.it=0
     @classmethod
     def from_config(cls, cfg, *args, **kwargs):
         # backbone
@@ -137,12 +141,12 @@ class YOLOv3(BaseArch):
         return self._forward()
 
     def get_loss_keys(self):
-        return ['loss_cls', 'loss_iou', 'loss_dfl']
+        return ['loss_cls', 'loss_iou', 'loss_dfl','loss_contrast']
 
     def get_distill_loss(self, head_outs, teacher_head_outs, ratio=0.01):
         # student_probs: already sigmoid
-        student_probs, student_deltas= head_outs
-        teacher_probs, teacher_deltas = teacher_head_outs
+        student_probs, student_deltas = head_outs
+        teacher_probs, teacher_deltas= teacher_head_outs
         nc = student_probs.shape[-1]
         student_probs = student_probs.reshape([-1, nc])
         teacher_probs = teacher_probs.reshape([-1, nc])
@@ -161,47 +165,51 @@ class YOLOv3(BaseArch):
             mask[sorted_inds[:count_num]] = 1.
             fg_num = sorted_vals[:count_num].sum()
             b_mask = mask > 0.
-
+        #comatch 
+            probs=teacher_probs[b_mask].detach()
+            temperature=0.2
+            alpha=0.9
+            if self.it>100: # memory-smoothing 
+                
+                    A = paddle.exp(paddle.mm(teacher_probs[b_mask], self.queue_probs.t())/temperature)       
+                    A = A/A.sum(1,keepdim=True)                    
+                    probs = alpha*probs + (1-alpha)*paddle.mm(A, self.queue_probs) 
+                # queue_ptr= student_probs.shape[0]
+            n = student_probs[b_mask].shape[0]   
+        sim = paddle.exp(paddle.mm(student_probs[b_mask], teacher_probs[b_mask].t())/0.2) #feats_u_s0.shape 448,64 sim.shape 448,448
+        sim_probs = sim / sim.sum(1, keepdim=True)
+        Q = paddle.mm(probs, probs.t())
+        # Q2 = paddle.mm(probs,  self.queue_probs.t())    
+        # paddle.cat([Q,Q2],dim=1)
+        Q.fill_diagonal_(1)    
+        pos_mask = (Q>=0.5).astype("float")
+            
+        Q = Q * pos_mask
+        Q = Q / Q.sum(1, keepdim=True)
+        # paddle.zeros([672,672]).fill_diagonal_(1)
+        # contrastive loss
+        #如果是多尺度的话建议直接建立一个list每间隔100个迭代pop(0),并且self.queue_=paddle.concat list
+        self.queue_feats[self.queue_ptr:self.queue_ptr + n,:] = teacher_probs[b_mask].detach()
+        self.queue_probs[self.queue_ptr:self.queue_ptr + n,:] = teacher_probs[b_mask].detach()
+        self.queue_ptr = (self.queue_ptr+n)%self.queue_size
+        self.it+=1
+        loss_contrast = - (paddle.log(sim_probs + 1e-7) * Q).sum(1)
+        loss_contrast = loss_contrast.mean()   
+        
+        
         loss_logits = QFLv2(
             student_probs, teacher_probs, weight=mask, reduction="sum") / fg_num
         # [88872, 80] [88872, 80]
+        
         iou_loss = GIoULoss(reduction='mean')
-        loss_deltas = iou_loss(student_deltas[b_mask], teacher_deltas[b_mask])
+        loss_deltas = iou_loss(student_deltas[b_mask],teacher_deltas[b_mask])
+
 
 
         return {
             "distill_loss_cls": loss_logits,
             "distill_loss_iou": loss_deltas,
             "distill_loss_dfl": paddle.to_tensor(0).astype('float32'),
+            "distill_loss_contrast": loss_contrast,
             "fg_sum": fg_num,
         }
-
-    def _df_loss(self, pred_dist, target):  # [810, 4, 17]  [810, 4]
-        target_left = paddle.cast(target, 'int64')
-        target_right = target_left + 1
-        weight_left = target_right.astype('float32') - target
-        weight_right = 1 - weight_left
-        loss_left = F.cross_entropy(
-            pred_dist, target_left, reduction='none') * weight_left
-        loss_right = F.cross_entropy(
-            pred_dist, target_right, reduction='none') * weight_right
-        return (loss_left + loss_right).mean(-1, keepdim=True)
-
-    def distribution_focal_loss(self,
-                                pred_corners,
-                                target_corners,
-                                weight_targets=None):
-        target_corners_label = F.softmax(target_corners, axis=-1)
-        loss_dfl = F.cross_entropy(
-            pred_corners,
-            target_corners_label,
-            soft_label=True,
-            reduction='none')
-        loss_dfl = loss_dfl.sum(1)
-        if weight_targets is not None:
-            loss_dfl = loss_dfl * (weight_targets.expand([-1, 4]).reshape([-1]))
-            loss_dfl = loss_dfl.sum(-1) / weight_targets.sum()
-        else:
-            loss_dfl = loss_dfl.mean(-1)
-        loss_dfl = loss_dfl / 4.  # 4 direction
-        return loss_dfl
